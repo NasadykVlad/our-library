77df6aeb9447011643af189f51d53cf0a9a070e2
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2022-01-20 17:02:27 +0200

    add-landing-page

--- frontend/src/components/landing/landing.js
+++ frontend/src/components/landing/landing.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import {Button, Carousel} from 'react-bootstrap';
 import './landing.scss'
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Landing</h1>
+        <div className="Landing" style={{backgroundImage: "url('./images/background-landing.png')"}}>
+            <h1>Читай Сьогодні, <br/> Читай Завтра</h1>
+
+            <h5>Веб-сервіс "Наша бібліотека" допоможе Вам у цьому!</h5>
+
+            <Button href='/register' variant="warning">Почніть користуватись особистим кабінетом просто зараз</Button>
+
+            <h5 class="why-us">Чому ви повинні вибрати нас?</h5>
+
+            <Carousel>
+                <Carousel.Item>
+                    <Carousel.Caption>
+                        <h3>Немає конкурентів на ринку</h3>
+                    </Carousel.Caption>
+                    <img
+                        className="d-block w-100"
+                        src="./images/books-slider-one.png"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="./images/books-slider-one.png"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Особисті кабінети користувачів</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="./images/books-slider-one.png"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Онлайн підтримка 24/7</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="./images/books-slider-one.png"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Безкоштовний доступ </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
         </div>
     );
 };
